d67fcc78a84051ae986d414b8871412de73edd9e
Timofey Yakimov <yakimov.timofey@gmail.com>
2019-12-05 09:14:50 +0500

    teacher16 Logging

---
+++ src/main/java/teacher16/inno20/MainLogback.java
@@ -0,0 +1,15 @@
+package teacher16.inno20;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainLogback {
+    private static final Logger logger = LoggerFactory.getLogger(MainLogback.class.getName());
+
+    public static void main(String[] args)  {
+        logger.info("HELLO");
+        logger.debug("DEBUG");
+        logger.trace("DEBUG");
+    }
+
+}
