d1f3699b98d44486cc93fe050bbce5459ce6bc1d
Timofey Yakimov <yakimov.timofey@gmail.com>
2019-12-08 17:10:49 +0500

    lesson12 JVM

--- src/main/java/lesson12/task2/MetaspaceTest.java
+++ src/main/java/lesson12/task2/MetaspaceTest.java
@@ -1,7 +1,7 @@
 package lesson12.task2;
 
-import javassist.ClassPool;
-import javassist.CannotCompileException;
+//import javassist.ClassPool;
+//import javassist.CannotCompileException;
 
 /**
  * Доработать программу так, чтобы ошибка OutOfMemoryError возникала в Metaspace /Permanent Generation
@@ -10,7 +10,7 @@ import javassist.CannotCompileException;
  */
 public class MetaspaceTest {
 
-    public static void main(String[] args) throws CannotCompileException {
+    public static void main(String[] args) {
         testPerm();
 //        testMetaspace();
     }
@@ -27,12 +27,12 @@ public class MetaspaceTest {
     // -XX:MaxMetaspaceSize=64m
     // JDK8
     // ClassPool из javassist
-    private static void testMetaspace() throws CannotCompileException {
-        ClassPool cp = ClassPool.getDefault();
-        int i=0;
-        while (true) {
-            Class c = cp.makeClass("lesson12.test2.Metaspace" + (i++)).toClass();
-        }
-    }
+//    private static void testMetaspace() throws CannotCompileException {
+//        ClassPool cp = ClassPool.getDefault();
+//        int i=0;
+//        while (true) {
+//            Class c = cp.makeClass("lesson12.test2.Metaspace" + (i++)).toClass();
+//        }
+//    }
 
 }
